4920c995793cc4bfc1b051b50274b5fae5c2b614
progress <leswifil@gmail.com>
2023-05-31 13:54:42 +0200

    functions

--- factory-functions/busTravelFunction.js
+++ factory-functions/busTravelFunction.js
@@ -1,50 +1,136 @@
 function busTravel() {
 
+
+    var pointsEntered = 0
+    var selectedLocation = ""
+    var singleTrips = 0
+    var pricePerSingleTrip = 0
+    var returnTrips = 0
+    var pricePerReturnTrip = 0
+    var enterPeakSeason = ""
+
+    var dunoonPrice = 25
+    var khayelitsaPrice = 40
+    var mitchellsPrice = 30
+    var dunoonPriceReturn = dunoonPrice * 2
+    var khayelitsaPriceReturn = khayelitsaPrice * 2
+    var mitchellsPricePeturn = mitchellsPrice * 2
+    var percentage = 25 / 100
+
+
     //function to get points
     function getPoints(points) {
-
+        pointsEntered = points
+        return pointsEntered
     }
 
     //function to get location
     function getLocation(location) {
-
+        selectedLocation = location
+        return selectedLocation
     }
 
+
+
+    // The price for a trip based on - travel time (peak or off-peak)
+    function peak(peakSeason) {
+        enterPeakSeason = peakSeason
+        return enterPeakSeason
+    }
+
+    //if off peak khayelitsa is R40, Dunoon is R25 and Mitchells plain is R30 for single trip
+    ////if off peak khayelitsa is R80, Dunoon is R50 and Mitchells plain is R60 for return trip
+    // if on peak each price for the location increases by 25% for single and return
     //number of trips
-    function numberOfSingleTrips(points, location){
+    function numberOfSingleTrips() {
         //return number of single trips based on points and location
         //while keeping in mind the price
+        if (pointsEntered && selectedLocation === "Dunoon" && enterPeakSeason === "offPeak") {
+            singleTrips = pointsEntered / dunoonPrice
+        } else if (pointsEntered && selectedLocation === "Mitchells" && enterPeakSeason === "offPeak") {
+            singleTrips = pointsEntered / mitchellsPrice
+        } else if (pointsEntered && selectedLocation === "Khayelitsha" && enterPeakSeason === "offPeak") {
+            singleTrips = pointsEntered / khayelitsaPrice
+        } else if (pointsEntered && selectedLocation === "Dunoon" && enterPeakSeason === "peak") {
+            singleTrips = pointsEntered / (dunoonPrice + (dunoonPrice * percentage))
+        } else if (pointsEntered && selectedLocation === "Mitchells" && enterPeakSeason === "peak") {
+            singleTrips = pointsEntered / (khayelitsaPrice + (khayelitsaPrice * percentage))
+        } else if (pointsEntered && selectedLocation === "Khayelitsha" && enterPeakSeason === "peak") {
+            singleTrips = pointsEntered / (mitchellsPrice + (mitchellsPrice * percentage))
+        }
+        return singleTrips.toFixed()
     }
-
     // The cost price per trip (single)
-    function pricePerTrip(location) {
+    function getPricePerTripSingle() {
         //calculate price per single trip
         // if location is ..
         //price is ..
+        if (selectedLocation === "Dunoon" && enterPeakSeason === "offPeak") {
+            pricePerSingleTrip = dunoonPrice
+        } else if (selectedLocation === "Mitchells" && enterPeakSeason === "offPeak") {
+            pricePerSingleTrip = mitchellsPrice
+        } else if (selectedLocation === "Khayelitsha" && enterPeakSeason === "offPeak") {
+            pricePerSingleTrip = khayelitsaPrice
+        } if (selectedLocation === "Dunoon" && enterPeakSeason === "peak") {
+            pricePerSingleTrip = dunoonPrice + (dunoonPrice * percentage)
+        } else if (selectedLocation === "Mitchells" && enterPeakSeason === "peak") {
+            pricePerSingleTrip = mitchellsPrice + (mitchellsPrice * percentage)
+        } else if (selectedLocation === "Khayelitsha" && enterPeakSeason === "peak") {
+            pricePerSingleTrip = khayelitsaPrice + (khayelitsaPrice * percentage)
+        }
+
+        return pricePerSingleTrip.toFixed(2)
     }
 
     //number of return trips
-    function numberOfReturnTrips(location,points){
+    function numberOfReturnTrips() {
         //return double the number of single trips based on points and location
         //while keeping in mind the price
+        if (pointsEntered && selectedLocation === "Dunoon" && enterPeakSeason === "offPeak") {
+            returnTrips = pointsEntered / dunoonPriceReturn
+        } else if (pointsEntered && selectedLocation === "Mitchells" && enterPeakSeason === "offPeak") {
+            returnTrips = pointsEntered / mitchellsPricePeturn
+        } else if (pointsEntered && selectedLocation === "Khayelitsha" && enterPeakSeason === "offPeak") {
+            returnTrips = pointsEntered / khayelitsaPriceReturn
+        } else if (pointsEntered && selectedLocation === "Dunoon" && enterPeakSeason === "peak") {
+            returnTrips = pointsEntered / (dunoonPriceReturn + (dunoonPriceReturn * percentage))
+        } else if (pointsEntered && selectedLocation === "Mitchells" && enterPeakSeason === "peak") {
+            returnTrips = pointsEntered / (mitchellsPricePeturn + (mitchellsPricePeturn * percentage))
+        } else if (pointsEntered && selectedLocation === "Khayelitsha" && enterPeakSeason === "peak") {
+            returnTrips = pointsEntered / (khayelitsaPriceReturn + (khayelitsaPriceReturn * percentage))
+        }
+
+        return returnTrips.toFixed()
     }
 
     // The cost price per trip (return)
-    function pricePerTripReturn(location) {
+    function pricePerTripReturn() {
         //calculate price per return trip
         // if location is ..
         //price is 2x ...
+        if (selectedLocation === "Dunoon" && enterPeakSeason === "offPeak") {
+            pricePerReturnTrip = dunoonPriceReturn;
+        } else if (selectedLocation === "Mitchells" && enterPeakSeason === "offPeak") {
+            pricePerReturnTrip = mitchellsPricePeturn;
+        } else if (selectedLocation === "Khayelitsha" && enterPeakSeason === "offPeak") {
+            pricePerReturnTrip = khayelitsaPriceReturn;
+        } else if (selectedLocation === "Dunoon" && enterPeakSeason === "peak") {
+            pricePerReturnTrip = dunoonPriceReturn + (dunoonPriceReturn * percentage);
+        } else if (selectedLocation === "Mitchells" && enterPeakSeason === "peak") {
+            pricePerReturnTrip = mitchellsPricePeturn + (mitchellsPricePeturn * percentage);
+        } else if (selectedLocation === "Khayelitsha" && enterPeakSeason === "peak") {
+            pricePerReturnTrip = khayelitsaPriceReturn + (khayelitsaPriceReturn * percentage);
+        }
+
+        return pricePerReturnTrip.toFixed(2)
     }
-
-    // The price for a trip based on - travel time (peak or off-peak)
-    function peakPrice(peak,offPeak){
-        //if off peak khayelitsa is R40, Dunoon is R25 and Mitchells plain is R30 for single trip
-        ////if off peak khayelitsa is R80, Dunoon is R50 and Mitchells plain is R60 for return trip
-        // if on peak each price for the location increases by 25% for single and return
-    }
-
-
     return {
-
+        getPoints,
+        getLocation,
+        getPricePerTripSingle,
+        numberOfReturnTrips,
+        pricePerTripReturn,
+        peak,
+        numberOfSingleTrips,
     }
 }
--- tests/bustTravel.test.js
+++ tests/bustTravel.test.js
@@ -4,3 +4,57 @@
 //number of return trips to take for each location
 //prices of trips for peak hours (single)
 //prices of trips for peak hours (return)
+describe('Bus Travel App', function(){
+    it("should get number of single trips during offpeak", function(){
+        var singleTrips = busTravel()
+
+        singleTrips.getPoints(100)
+        singleTrips.getLocation("Dunoon")
+        singleTrips.peak("offPeak")
+
+        assert.equal(4 ,singleTrips.numberOfSingleTrips())
+    })
+    it("should get number of single trips during peak", function(){
+        var singleTrips = busTravel()
+
+        singleTrips.getPoints(100)
+        singleTrips.getLocation("Dunoon")
+        singleTrips.peak("peak")
+
+        assert.equal(3 ,singleTrips.numberOfSingleTrips())
+    })
+    it("should get prices for single trip during offpeak", function(){
+        var singleTrips = busTravel()
+
+        singleTrips.getLocation("Dunoon")
+        singleTrips.peak("offPeak")
+
+        assert.equal(25 ,singleTrips.getPricePerTripSingle())
+    })
+    it("should get prices for single trip during peak", function(){
+        var singleTrips = busTravel()
+
+        singleTrips.getLocation("Dunoon")
+        singleTrips.peak("peak")
+
+        assert.equal(31.25 ,singleTrips.getPricePerTripSingle())
+    })
+    it("should get number of return trips during peak", function(){
+        var returnTrips = busTravel()
+
+        returnTrips.getPoints(100)
+        returnTrips.getLocation("Dunoon")
+        returnTrips.peak("peak")
+
+        assert.equal(2 ,returnTrips.numberOfReturnTrips())
+    })
+    it("should get number of single trips during offpeak", function(){
+        var singleTrips = busTravel()
+
+        singleTrips.getPoints(100)
+        singleTrips.getLocation("Dunoon")
+        singleTrips.peak("offPeak")
+
+        assert.equal(2 ,singleTrips.numberOfReturnTrips())
+    })
+})
